16d469138af3bc665725e120b8b60f25bc9664f4
shruthiamar <>
2026-03-24 21:40:22 +0000

    DataFrame Inspection

--- analyzing_the_data/data_frame_inspection/main.py
+++ analyzing_the_data/data_frame_inspection/main.py
@@ -3,7 +3,9 @@ import pandas as pd
 wine_data = pd.read_csv('https://codefinity-content-media.s3.eu-west-1.amazonaws.com/a43d24b6-df61-4e11-9c90-5b36552b3437/wine.csv')
 
 # Write your code below
-information = wine_data.info()
+column_names = wine_data.columns
+data_types = wine_data.dtypes
 
 # Testing the result
-print(information)
+print(column_names)
+print(data_types)
